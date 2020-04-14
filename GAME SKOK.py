26deb0bfe6e222f0d59664465187e90ae2c7760f
Oleg <qqq@qq.com>
2020-04-14 17:36:03 +0300

    adding_lives

--- GAME SKOK.py
+++ GAME SKOK.py
@@ -3,14 +3,6 @@ import time
 import random
 from tkinter import messagebox as mb
 
-t = Tk()
-t.title("Game Skok")
-t.resizable(0, 0)
-t.wm_attributes("-topmost", 1)
-c = Canvas(t, width = 800, height = 600, bd = 0, highlightthickness=0)
-c.pack()
-t.update()
-
 class ball:
     def __init__(self, canvas, rack, color):
         self.canvas = canvas
@@ -44,7 +36,7 @@ class ball:
             if pos [3] >= rack_pos[1] and pos [1] <= rack_pos[3]:
                 return True
         return False
-    
+
 class racket:
     def __init__(self, canvas, color):
         self.canvas = canvas
@@ -70,16 +62,44 @@ class racket:
         self.x = -10
         self.draw('left')
  
+
+t = Tk()
+t.title("Game Skok")
+t.resizable(0, 0)
+t.wm_attributes("-topmost", 1)
+c = Canvas(t, width = 800, height = 600, bd = 0, highlightthickness=0)
+c.pack()
+t.update()    
+
 rack = racket(c, "blue")
 Ball = ball(c, rack, "red")
-
-
-
+life = 5
+c.create_text(400,20, text = 'lives left = %s' % life, font = ('Times', 18), tag = 'delete')
 while 1:
     Ball.draw()
-    if Ball.bottom == True:
-        mb.showinfo("wrong404","Game Over")
-        break
+    if Ball.bottom == True and life >= 2:
+        life = life - 1
+        c.delete('delete')
+        c.create_text(400,20, text = 'lives left = %s' % life, font = ('Times', 18), tag = 'delete')
+        Ball.bottom = False
+        c.coords(Ball.id,10,10, 25, 25)
+        c.move(Ball.id, 400, 300)
+        Ball.x = random.randint(-5,-1)
+        Ball.y = random.randint(-5,-1)
+        mb.showinfo("The ball is fall down","Try againe")
     t.update_idletasks()
     t.update()
     time.sleep(0.01)
+    if Ball.bottom == True and life == 1:
+        life = life - 1
+        c.delete('delete')
+        c.create_text(400,20, text = 'lives left = %s' % life, font = ('Times', 18), tag = 'delete')
+        Ball.bottom = False
+        c.coords(Ball.id,10,10, 25, 25)
+        c.move(Ball.id, 400, 300)
+        Ball.x = random.randint(-5,-1)
+        Ball.y = random.randint(-5,-1)
+    if life == 0:
+        mb.showinfo("Oh, no!","you lost")
+        break
+        
